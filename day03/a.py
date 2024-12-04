ffe8e4b7913c0e1b5f21eeccc6b9c2cbeef6d613
Albert Ji <peleusj@gmail.com>
2024-12-04 10:19:21 +0800

    day 03 part 2

--- day03/a.py
+++ day03/a.py
@@ -2,27 +2,23 @@ import sys
 import re
 
 with open(sys.argv[1]) as file:
-    text = file.read().strip()
+    lines = file.read().strip()
 
-p1_pattern = r"mul\((\d+),(\d+)\)"
-p2_pattern = r"do\(\).*?(?:don\'t\(\))"
+pattern = r"mul\((\d+),(\d+)\)"
+enabled = r"do\(\)([\s\S]*?)(?:don\'t\(\)|$)"
 
-p1_matches = re.findall(p1_pattern, text)
-
-p2_text = re.findall(p2_pattern, "do()" + text)
+p2_text = re.findall(enabled, "do()" + lines)
 
 p1 = 0
-
+p1_matches = re.findall(pattern, lines)
 for match in p1_matches:
     p1 += int(match[0]) * int(match[1])
 
 p2 = 0
-
 for text in p2_text:
-    print(text)
-    # matches = re.findall(p1_pattern, p2_match)
-    # for match in matches:
-    #     p2 += int(match[0]) * int(match[1])
+    matches = re.findall(pattern, text)
+    for match in matches:
+        p2 += int(match[0]) * int(match[1])
 
 print(f"Part 1: {p1}")
 print(f"Part 2: {p2}")
